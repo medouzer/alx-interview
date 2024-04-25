59bd7d84a1ed321f87d694c89de735c203242752
mouzerzo <mouzerzo@e1r4p12.1337.ma>
2024-04-25 01:00:20 +0100

    new task

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+0-pascale_triangle.py
+"""
+
+def make_arr(arr, n):
+    table = [1]
+    if(n == 0):
+        return (table)
+    for index in range(1, n):
+        table.append(arr[index - 1] + arr[index ])
+    table.append(1)
+    return (table)
+
+def pascal_triangle(n):
+    res = [[1]]
+    for index in range(1, n):
+        res.append( make_arr(res[index - 1], index) )
+    return (res)
