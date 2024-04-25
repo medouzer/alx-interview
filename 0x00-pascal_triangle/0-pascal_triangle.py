b754be86a66c3b40cc7f0955611b59dbfcf502ec
mohamed <mohamed.ouz2016@gmail.com>
2024-04-25 22:28:21 +0000

    new things

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,16 +3,18 @@
 0-pascale_triangle.py
 """
 
+
 def make_arr(arr, n):
     """this also"""
-    
+
     table = [1]
     if(n == 0):
         return (table)
     for index in range(1, n):
-        table.append(arr[index - 1] + arr[index ])
+        table.append(arr[index - 1] + arr[index])
     table.append(1)
     return (table)
+
 
 def pascal_triangle(n):
     """this function to"""
@@ -21,5 +23,5 @@ def pascal_triangle(n):
         return []
     res = [[1]]
     for index in range(1, n):
-        res.append( make_arr(res[index - 1], index) )
+        res.append(make_arr(res[index - 1], index))
     return (res)
