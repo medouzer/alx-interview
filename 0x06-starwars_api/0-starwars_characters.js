2e229dabfda9d459a37239fed151d84712ac8754
ouzerzou <mohamed.ouz2016@gmail.com>
2024-06-06 19:52:30 +0100

    new things

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,19 +5,30 @@ const request = require('request')
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2]
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error)
-  } else {
-    const characters = JSON.parse(body).characters
-    for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.error(error)
-        } else {
-          console.log(JSON.parse(body).name)
-        }
-      })
+const get_characters = (url) => {
+  request(url, async (err, res, body) => {
+    if (err) {
+      console.log(err)
+    } else {
+      const characters = JSON.parse(body).characters
+      for (const character of characters) {
+        const name = await get_name(character)
+        console.log(name)
+      }
     }
-  }
-})
+  })
+}
+
+const get_name = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(JSON.parse(body).name)
+      }
+    })
+  })
+}
+
+get_characters(url)
