dfa364bf301402eba5644a2da59bbe222262c3cd
ouzerzou <mohamed.ouz2016@gmail.com>
2024-06-06 19:43:26 +0100

    new things

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+const request = require('request')
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2]
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error)
+  } else {
+    const characters = JSON.parse(body).characters
+    for (const character of characters) {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.error(error)
+        } else {
+          console.log(JSON.parse(body).name)
+        }
+      })
+    }
+  }
+})
