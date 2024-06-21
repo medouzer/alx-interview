5736e5765cbd2e73db0664b4abb318ac907e314e
ouzerzou <mohamed.ouz2016@gmail.com>
2024-06-21 01:14:16 +0100

    new things

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,10 +7,10 @@ def makeChange(coins, total):
     to meet a given amount total"""
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
+    dp = [total + 1] * (total + 1)
     dp[0] = 0
     for amount in range(1, total + 1):
         for coin in coins:
             if coin <= amount:
                 dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != total + 1 else -1
