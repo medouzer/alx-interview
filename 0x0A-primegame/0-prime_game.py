7a593dd70bf97e1e0d9219c0bcb22c37d8fb0ebd
ouzerzou <mohamed.ouz2016@gmail.com>
2024-07-05 02:41:47 +0100

    check 6 and 9

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,34 +2,30 @@
 """Prime Game"""
 
 
-def play_game(n):
-    """play_game function"""
-    if n < 2:
-        return 1
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False
-    p = 2
-    while p * p <= n:
-        if is_prime[p]:
-            for i in range(p * p, n + 1, p):
-                is_prime[i] = False
-        p += 1
-    primes = [num for num, prime in enumerate(is_prime) if prime]
-    turn = 0
-    while primes:
-        turn += 1
-        primes = [prime for prime in primes if prime % primes[0] != 0]
-        primes = primes[1:]
-    return turn % 2
-
-
 def isWinner(x, nums):
     """isWinner function"""
+    def play_game(n):
+        """play_game function"""
+        if n < 2:
+            return 1
+        is_prime = [True] * (n + 1)
+        is_prime[0] = is_prime[1] = False
+        p = 2
+        while p * p <= n:
+            if is_prime[p]:
+                for i in range(p * p, n + 1, p):
+                    is_prime[i] = False
+            p += 1
+        primes = [num for num, prime in enumerate(is_prime) if prime]
+        turn = 0
+        while primes:
+            turn += 1
+            primes = [prime for prime in primes if prime % primes[0] != 0]
+            primes = primes[1:]
+        return turn % 2
     maria_wins = 0
     ben_wins = 0
 
-    if x < 1 or not nums:
-        return None
     for i in range(x):
         n = nums[i]
         if play_game(n) == 0:
